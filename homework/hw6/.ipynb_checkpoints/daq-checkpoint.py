ea5d39dbe628c7ac4863dae2e3311a1892daaf7d
David James <davidabraham@ucla.edu>
2019-02-25 09:18:46 -0800

    start of hw6 added; daq module created, untested

---
+++ homework/hw6/.ipynb_checkpoints/daq-checkpoint.py
@@ -0,0 +1,47 @@
+from time import sleep
+from pyfirmata import Arduino, util
+
+def activateHal():
+    port = '/dev/ttyUSB0'
+    hal9001 = Arduino(port)
+    
+    it = util.Iterator(hal9001)
+    it.start()
+    
+    channels = []
+    
+    chan0 = hal9001.get_pin('a:0:i')
+    sleep(5)
+    channels.append(chan0)
+    
+    chan1 = hal9001.get_pin('a:1:i')
+    sleep(5)
+    channels.append(chan1)
+    
+    chan2 = hal9001.get_pin('a:2:i')
+    sleep(5)
+    channels.append(chan2)
+    
+    chan3 = hal9001.get_pin('a:3:i')
+    sleep(5)
+    channels.append(chan3)
+    
+    chan4 = hal9001.get_pin('a:4:i')
+    sleep(5)
+    channels.append(chan4)
+    
+    chan5 = hal9001.get_pin('a:5:i')
+    sleep(5)
+    channels.append(chan5)
+    
+    for i in channels:
+        i.enable_reporting()
+        print(i.read()*5,sep='/n')
+
+    print('Readings nominal')
+    
+    return hal9001,channels
+
+def readHal(channels):
+    return [i.read()*5 for i in channels]
+
